a0bfddc44e20e171d00c2ef8fb3792e9df795e2b
pltaillat56 <152601929+pltaillat56@users.noreply.github.com>
2023-12-08 10:06:29 +0100

    Add files via upload

---
+++ work/conv2maxent.py
@@ -0,0 +1,14 @@
+sigma = 0.0007
+inf = open('Gloc.dat','r')
+no_orb = 1
+useorb = 0
+
+outf = open('gf.dat','w')
+for line in inf:
+   data = [float(x) for x in line.split()]
+	orb_index=2*useorb*no_orb + 2*useorb
+   outf.write( str(data[0])+ '\t' + str( (data[1+orb_index]) ) + '\t'  + str(sigma) + '\t' \
+                                  + str( (data[2+orb_index]) ) + '\t'  + str(sigma) + '\n' )
+
+outf.close()
+inf.close()
